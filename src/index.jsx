9574d64c6520e0232dc55cd820c06368fd1f1201
Marco Gallegos <marcogs1202@gmail.com>
2022-10-13 13:45:27 -0500

    fix router SPA  with hashrouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import "./styles.css"
 import {JournalApp}  from './JournalApp';
 import { Provider } from 'react-redux';
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <JournalApp/>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
